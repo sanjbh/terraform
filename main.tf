1c7e62cabdd1e203f911d06afda22590f521e6cb
Sanjay Bhattacahrya <sanjbh@gmail.com>
2025-01-16 18:10:11 +0530

    Added security group

--- main.tf
+++ main.tf
@@ -4,7 +4,31 @@ provider "aws" {
 
 
 resource "aws_instance" "example" {
-  ami           = data.aws_ami.amazon-linux.id
-  instance_type = var.instance_type
+  ami                    = data.aws_ami.amazon-linux.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
+  user_data = <<-EOF
+    #!/bin/sh
+    echo "Hello, world" > index.xhtml
+    nohup busybox httpd -f -p 8080 &
+  EOF
+
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "terraform-example"
+  }
+
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
